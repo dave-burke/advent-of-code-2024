8a5613989838c3eb2d836805ac56794a8d025079
Dave Burke <dburke84@gmail.com>
2024-12-01 18:05:47 -0600

    Implement day01 part01

--- day01.rb
+++ day01.rb
@@ -4,6 +4,20 @@ require 'net/http'
 require_relative 'aoc'
 
 Aoc.download_input_if_needed(1)
-File.readlines('day01.txt').each_with_index do |line, _i|
-  puts line
+
+lines = File.readlines('day01.txt')
+
+pairs = lines.map do |line|
+  /(\d+)   (\d+)/.match(line).to_a.drop(1).map(&:to_i)
 end
+
+left, right = pairs.transpose
+
+left.sort!
+right.sort!
+
+sorted_pairs = [left, right].transpose
+
+diffs = sorted_pairs.map { |l, r| (l - r).abs }
+
+puts diffs.sort!.sum
