acdd79138efe64332f14509cf39997d7a839bac2
Dave Burke <dburke84@gmail.com>
2024-12-03 07:44:16 -0600

    Implement day03 part02

--- day03.rb
+++ day03.rb
@@ -15,9 +15,23 @@ def part1(input)
 end
 
 def part2(input)
-  puts 'not implemented'
-  nil if input.nil?
+  matches = input.scan(/(do\(\))|(don't\(\))|mul\((\d+),(\d+)\)/)
+  enabled = true
+  products = []
+  matches.each do |match|
+    on, off, a, b = match
+    unless on.nil?
+      enabled = true
+      next
+    end
+    unless off.nil?
+      enabled = false
+      next
+    end
+    products.push(a.to_i * b.to_i) if enabled
+  end
+  puts products.sum
 end
 
 input = Aoc.download_input_if_needed(DAY)
-part1(input)
+part2(input)
