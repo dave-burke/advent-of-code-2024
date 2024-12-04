2fcf0596df1a033796c8e3a63063d08faf1b83de
Dave Burke <dburke84@gmail.com>
2024-12-04 07:43:14 -0600

    Raise error if downloading day 0 input

--- aoc.rb
+++ aoc.rb
@@ -14,17 +14,24 @@ module Aoc
     end
   end
 
+  def self.download_input(year, day, cookie)
+    headers = { Cookie: cookie }
+    uri = "https://adventofcode.com/#{year}/day/#{day}/input"
+    puts "GET #{uri}"
+    Net::HTTP.get(URI(uri), headers)
+  end
+
   def self.download_input_if_needed(day)
+    raise 'Day must be a positive integer' unless day.positive?
+
     file_name = "input#{day.to_s.rjust(2, '0')}.txt"
     return File.read(file_name) if File.exist? file_name
 
     year = determine_year
     cookie = File.read('session.txt')
-    headers = { Cookie: cookie }
-    uri = "https://adventofcode.com/#{year}/day/#{day}/input"
-    puts "GET #{uri} > #{file_name}"
-    input = Net::HTTP.get(URI(uri), headers)
+    input = download_input(year, day, cookie)
     File.write(file_name, input)
+    puts "Input saved as #{file_name}"
 
     input
   end
