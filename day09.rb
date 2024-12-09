e9613c1812c9592714ed2c64cfb321aac504eeca
Dave Burke <dburke84@gmail.com>
2024-12-09 16:22:21 -0600

    Implement day09 part01 -- works on example, but not input

--- day09.rb
+++ day09.rb
@@ -8,33 +8,101 @@ DAY = 9
 
 ## Represents a contiguous block for data for a single file
 class Block
-  def initialize(id, length)
+  def initialize(id, count)
     @id = id
-    @length = length
+    @count = count.to_i
   end
 
-  attr_reader :id, :length
+  attr_reader :id, :count
+
+  def with_count(new_count)
+    Block.new(@id, new_count)
+  end
+
+  def with_id(new_id)
+    Block.new(new_id, @count)
+  end
+
+  def to_debug_s
+    "{#{@id}x#{@count}}"
+  end
 
   def to_s
-    "{#{@id}x#{@length}}"
+    return '.' * @count if @id.nil?
+
+    @id * @count
   end
 end
 
-def part1(input)
+def parse_blocks(input)
   is_file = true
   next_id = 0
   blocks = []
   input.strip.chars.each do |char|
     if is_file
-      blocks << Block.new(next_id, char)
+      blocks << Block.new(next_id.to_s, char)
       next_id += 1
-    else
+    elsif char != '0'
       blocks << Block.new(nil, char)
     end
     is_file = !is_file
   end
+  blocks
+end
 
-  puts "#{blocks.map(&:to_s)}"
+def fill_next(blocks)
+  blocks = blocks.dup
+  next_nil_i = blocks.index { |block| block.id.nil? }
+  last_char_i = blocks.rindex { |block| !block.id.nil? && block.count.positive? }
+
+  return nil if next_nil_i > last_char_i # done
+
+  next_nil = blocks[next_nil_i]
+  last_char = blocks[last_char_i]
+
+  if next_nil.count > last_char.count
+    # Can move entire end block
+    blocks.pop
+    remainder = next_nil.count - last_char.count
+    blocks[next_nil_i] = next_nil.with_count(remainder)
+    blocks.insert(next_nil_i, last_char)
+    blocks.push(Block.new(nil, next_nil.count - remainder))
+  else
+    # Can only move part of the end block
+    blocks[next_nil_i] = next_nil.with_id(last_char.id)
+    blocks[last_char_i] = last_char.with_count(last_char.count - next_nil.count)
+    blocks.push(Block.new(nil, next_nil.count))
+  end
+  blocks
+end
+
+def checksum(blocks)
+  i = 0
+  result = 0
+  blocks.each do |block|
+    break if block.id.nil?
+
+    (0..block.count - 1).each do |_|
+      result += block.id.to_i * i
+      i += 1
+    end
+  end
+  result
+end
+
+def part1(input)
+  # puts input
+  blocks = parse_blocks(input)
+
+  final = nil
+  until blocks.nil?
+    next_blocks = fill_next(blocks)
+    final = blocks if next_blocks.nil?
+    blocks = next_blocks
+  end
+  # p final.map(&:to_s).join('|')
+  puts checksum(final)
+  # 14569089796750 is too high
 end
 
 def part2(input)
