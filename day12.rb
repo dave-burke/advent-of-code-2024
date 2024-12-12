28d8cf89908ccd24e7e97460596465747b9058cd
Dave Burke <dburke84@gmail.com>
2024-12-12 07:03:33 -0600

    Complete day12 part01

--- day12.rb
+++ day12.rb
@@ -88,31 +88,29 @@ def part1(input)
     end
   end
 
-  plot = flood(rows[0][0])
-  puts plot.map(&:to_s)
-  puts plot.length
-  exit
+  plotted = Set.new
+  plots = []
+  rows.each_with_index do |row, r|
+    row.each_with_index do |col, c|
+      next if plotted.include?(col)
 
-  gardens = {}
-  rows.each do |row|
-    row.each do |col|
-      gardens[col.value] = [] unless gardens.key? col.value
-      gardens[col.value].push(col)
-      # puts "#{col.value} => #{gardens[col.value].map(&:to_s)}"
+      plot = flood(col)
+      plots.push(plot)
+      plotted.merge(plot)
     end
   end
 
   result = 0
-  gardens.each do |char, plots|
-    total_perimiter = plots.map(&:perimiter).map(&:length).sum
-    area = plots.length
+  plots.each do |plot|
+    total_perimiter = plot.map(&:perimiter).map(&:length).sum
+    area = plot.length
     price = total_perimiter * area
     # puts "#{char} => #{plots.map(&:to_s)}"
-    puts "#{char} has area #{area} and perimiter #{total_perimiter} for price #{price}"
+    puts "#{plot.first.value} has area #{area} and perimiter #{total_perimiter} for price #{price}"
     result += price
   end
 
-  puts "Found #{gardens.length} letters"
+  puts "Found #{plots.length} gardens"
 
   # Expected: 1930 for example input
   puts result
