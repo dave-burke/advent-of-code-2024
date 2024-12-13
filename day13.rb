03c1a714c3d2a51d4ae7d2b1817673bbf92c22c0
Dave Burke <dburke84@gmail.com>
2024-12-13 13:02:16 -0600

    Implement input parsing

--- day13.rb
+++ day13.rb
@@ -6,9 +6,50 @@ require_relative 'aoc'
 
 DAY = 13
 
+## Button A: X+[delta_x], Y+[delta_y]
+class Button
+  def initialize(spec)
+    _, @name, @delta_x, @delta_y = /Button (.): X\+(\d+), Y\+(\d+)/.match(spec).to_a
+  end
+
+  def to_s
+    "Button #{@name}: X+#{@delta_x}, Y+#{@delta_y}"
+  end
+end
+
+## Prize: X=[x_pos], Y=[y_pos]
+class Prize
+  def initialize(spec)
+    _, @x_pos, @y_pos = /Prize: X=(\d+), Y=(\d+)/.match(spec).to_a
+  end
+
+  def to_s
+    "Prize: X=#{@x_pos}, Y=#{@y_pos}"
+  end
+end
+
+class Machine
+  def initialize(spec)
+    a_spec, b_spec, prize_spec = spec.split("\n")
+    @button_a = Button.new(a_spec)
+    @button_b = Button.new(b_spec)
+    @prize_spec = Prize.new(prize_spec)
+  end
+
+  def to_s
+    "#{@button_a}\n#{@button_b}\n#{@prize_spec}\n"
+  end
+end
+
 def part1(input)
-  lines = input.split("\n")
-  puts "There were #{lines.size} lines"
+  specs = input.split("\n\n")
+
+  machines = specs.map { Machine.new(_1) }
+
+  machines.each do |machine|
+    puts machine
+    puts ''
+  end
 end
 
 def part2(input)
