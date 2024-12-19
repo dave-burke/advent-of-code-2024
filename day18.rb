98f58ae890739d515e57d3f2f15bcf102bc590b6
Dave Burke <dburke84@gmail.com>
2024-12-18 19:06:53 -0600

    Init day18

---
+++ day18.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'logger'
+require 'net/http'
+require_relative 'aoc'
+
+DAY = 18
+
+LOG = Logger.new($stdout, level: Logger::DEBUG)
+LOG.formatter = proc do |severity, datetime, _, msg|
+  "#{datetime.strftime('%H:%M:%S')} #{severity.ljust(5)}: #{msg}\n"
+end
+
+def part1(input)
+  lines = input.split("\n")
+  LOG.info("There were #{lines.size} lines")
+end
+
+def part2(input)
+  LOG.warn('not implemented')
+  nil if input.nil?
+end
+
+input = Aoc.download_input_if_needed(DAY)
+part1(input)
